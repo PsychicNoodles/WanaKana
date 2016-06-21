a001af771ade55ffafea76041f7ca9ab9c0849a4
Birnbaum, Mattori <Birnbaum.Mattori@principal.com>
2016-06-21 15:35:28 -0500

    updated syllable permutations to also change vowel, added docs to each test var

--- __tests__/utils-test.js
+++ __tests__/utils-test.js
@@ -12,22 +12,34 @@ let consonants   = ['ㄱ', 'ㄴ', 'ㄷ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅅ', 'ㅆ', 'ㅇ
 let dubCons      = ['ㄲ', 'ㄳ', 'ㄵ', 'ㄶ', 'ㄸ', 'ㄺ', 'ㄻ', 'ㄼ', 'ㄽ', 'ㄾ', 'ㄿ',
                     'ㅀ', 'ㅃ', 'ㅄ']
 
-let withFinals   = ['각', '깎', '갃', '난', '낝', '낞', '닫', '랄', '랅', '랆', '랇',
-                    '랈', '랉', '랊', '랋', '맘', '밥', '밦', '삿', '쌌', '앙', '잦',
+// every possible final consonant with either corresponding initial consonant or
+// non-doubled version
+// also every vowel, with duplicates on the last 6 syllables with, just because
+// there are fewer vowels than possible final consonants/double consonants
+let withFinals   = ['각', '깪', '갻', '냰', '넍', '넪', '뎓', '롈', '롥', '뢂', '뢟',
+                    '뢼', '룙', '룶', '뤓', '뭼', '뷥', '븂', '슷', '씠', '잉', '잦',
                     '찿', '캌', '탙', '팦', '핳']
 // these indeed may have duplicates, but they're parallel to withFinals
-let woFinals     = ['가', '까', '가', '나', '나', '나', '다', '라', '라', '라', '라',
-                    '라', '라', '라', '라', '마', '바', '바', '사', '싸', '아', '자',
+// the same syllables as withFinals, but without final consonants
+let woFinals     = ['가', '깨', '갸', '냬', '너', '네', '뎌', '례', '로', '롸', '뢔',
+                    '뢰', '료', '루', '뤄', '뭬', '뷔', '뷰', '스', '씌', '이', '자',
                     '차', '카', '타', '파', '하']
+// the final consonants from withFinals
 let consFinals   = ['ㄱ', 'ㄲ', 'ㄳ', 'ㄴ', 'ㄵ', 'ㄶ', 'ㄷ', 'ㄹ', 'ㄺ', 'ㄻ', 'ㄼ',
                     'ㄽ', 'ㄾ', 'ㄿ', 'ㅀ', 'ㅁ', 'ㅂ', 'ㅄ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ',
                     'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ']
+// the initial consonants from withFinals
 let consInitials = ['ㄱ', 'ㄲ', 'ㄱ', 'ㄴ', 'ㄴ', 'ㄴ', 'ㄷ', 'ㄹ', 'ㄹ', 'ㄹ', 'ㄹ',
                     'ㄹ', 'ㄹ', 'ㄹ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅂ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ',
                     'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ']
-let rplInitials  = ['학', '팎', '탃', '칸', '찭', '잖', '앋', '쌀', '삵', '밞', '밟',
-                    '맔', '랉', '랊', '랋', '람', '랍', '랎', '랏', '랐', '당', '낮',
-                    '낯', '낰', '같', '깦', '갛'] //withFinals initials replaced by consInitials reversed
+// the vowels from withFinals
+let onlyVowels   = ['ㅏ', 'ㅐ', 'ㅑ', 'ㅒ', 'ㅓ', 'ㅔ', 'ㅕ', 'ㅖ', 'ㅗ', 'ㅘ', 'ㅙ',
+                    'ㅚ', 'ㅛ', 'ㅜ', 'ㅝ', 'ㅞ', 'ㅟ', 'ㅠ', 'ㅡ', 'ㅢ', 'ㅣ', 'ㅏ',
+                    'ㅏ', 'ㅏ', 'ㅏ', 'ㅏ', 'ㅏ']
+// withFinals initials replaced by consInitials in reverse order
+let rplInitials  = ['학', '팪', '탻', '컌', '첝', '젢', '엳', '쏄', '솕', '봚', '봴',
+                    '묈', '룙', '룶', '뤓', '뤰', '륍', '륪', '릇', '릤', '딩', '낮',
+                    '낯', '낰', '같', '깦', '갛']
 
 let withFinalsIndicies = Array(withFinals.length).fill().map((_, i) => i)
 
