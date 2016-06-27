424a6b4655c8c86ac8dd9d5e99dd63bd82ecab40
Birnbaum, Mattori <Birnbaum.Mattori@principal.com>
2016-06-27 16:45:11 -0500

    began adding consonant pronunciation rule tests

--- __tests__/wanakana-test.js
+++ __tests__/wanakana-test.js
@@ -3,6 +3,7 @@ jest.unmock('../src/coffee/utils')
 
 import {toRomaja} from '../src/coffee/wanakana'
 
+// lone syllables, so without any special rules
 let syllables      = ['각', '깪', '갻', '냰', '넍', '넪', '뎓', '롈', '롥', '뢂', '뢟',
                       '뢼', '룙', '룶', '뤓', '뭼', '뷥', '븂', '슷', '씠', '잉', '잦',
                       '찿', '캌', '탙', '팦', '핳']
@@ -10,6 +11,11 @@ let romans         = ['gak', 'kkaekk', 'gyak', 'nyaen', 'neon', 'nen', 'dyeot',
                       'ryel', 'rol', 'rwam', 'rael', 'roel', 'ryol', 'rup', 'rwol',
                       'mwem', 'bwip', 'byup', 'seut', 'ssuit', 'ing', 'jat', 'chat',
                       'kak', 'tat', 'pap', 'hat']
+// consonant pronunciation tests sourced largely from the Integrated Korean
+// Beginning Level 1 textbook, except when an important case was not provided
+let resylInit      = ['한글은', '책을', '알았어요', '질묵이']
+let resylRes       = ['한그른', '채글', '아라써요', '질무기']
+// groups of the syllables with consonant finals used above
 let words          = ['각팦갻', '냰깪넪', '뎓넍롥', '뢂롈뢼', '룙뢟뤓', '뭼룶븂',
                       '슷뷥잉', '잦씠캌', '탙찿핳']
 let romanWords     = ['gak-pap-kkyak', 'nyaen-kkaeng-nen', 'dyeon-neol-lok',
